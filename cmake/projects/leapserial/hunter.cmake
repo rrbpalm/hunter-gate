b82b60fbe172ce965f98f4b7c4cff4f2ba35cdee
James Donald <jdonald.x@gmail.com>
2019-02-12 13:32:02 -0800

    Add a sample LeapSerial configuration

---
+++ cmake/projects/leapserial/hunter.cmake
@@ -0,0 +1,25 @@
+# Copyright (c) 2019, Redrock Biometrics
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+# Load used modules
+include(hunter_add_package)
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+# List of versions here...
+hunter_add_version(
+    PACKAGE_NAME
+    LeapSerial
+    VERSION
+    "0.5.2"
+    URL
+    "https://github.com/leapmotion/leapserial/archive/v0.5.2.tar.gz"
+    SHA1
+    afbe5038b59db8f0c54712d734ed0088232cfdee
+)
+
